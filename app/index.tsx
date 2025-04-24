5eed8c89985de96717de239a60b35b247dff51fd
Ian Yeh <its.ian.y07@gmail.com>
2025-04-23 23:17:31 -0400

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="font-bold text-[#bf0912] text-3xl text-center">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
